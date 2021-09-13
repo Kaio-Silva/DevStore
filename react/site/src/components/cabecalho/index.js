29934a628d04de34456d913eeabb69f73c0aeb5b
luigi <luigidasilvacoelho@gmail.com>
2021-09-12 22:59:39 -0300

    ok

--- react/site/src/components/cabecalho/index.js
+++ react/site/src/components/cabecalho/index.js
@@ -1,10 +1,18 @@
 
 import { Container } from './styled'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Index() {
+
+    async function emBreve(){
+        toast.warning("Em breve...")
+    }
+
     return (
         <Container>
+            <ToastContainer />
             <div class="reader-right-box">
                 <div class="box-user"> 
                     <div class="user-image">
@@ -15,8 +23,8 @@ export default function Index() {
                 </div>
                 
                 <div class="box-image">
-                    <div class="refresh-button"> <button> <img src="/assets/images/refresh.svg" alt = "" />  </button> </div>
-                    <div class="left-button"> <button> <img src="/assets/images/log-out.svg" alt = "" />  </button> </div>
+                    <div class="refresh-button"> <button onClick={emBreve}> <img src="/assets/images/refresh.svg" alt = "" />  </button> </div>
+                    <div class="left-button"> <button onClick={emBreve}> <img src="/assets/images/log-out.svg" alt = "" />  </button> </div>
                 </div>
             </div>
             <div class="bottom-bar-right-header" />
